2a22b9edb059e908329d7c7892f6ff00a32ecb13
marodev <modermatt@tuta.io>
2021-10-18 22:02:05 +0200

    :rotating_light: Add additional regression for OfType<>

--- test/EagleRepair.IntegrationTests/Cli/DataProvider/SimplifyIEnumerableLinq/SimplifyIEnumerableLinqOfTypeDataProvider.cs
+++ test/EagleRepair.IntegrationTests/Cli/DataProvider/SimplifyIEnumerableLinq/SimplifyIEnumerableLinqOfTypeDataProvider.cs
@@ -4,7 +4,7 @@ namespace EagleRepair.IntegrationTests.Cli.DataProvider.SimplifyIEnumerableLinq
 {
     public class SimplifyIEnumerableLinqOfTypeDataProvider
     {
-        private const string Input = @"
+        private const string Input1 = @"
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,7 +19,19 @@ namespace Entry
     }
 }";
 
-        private const string ExpectedOutput = @"
+        private const string Input2 = @"
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entry
+{
+    public class C
+    {
+        public static IEnumerable<T> Foo<T>(ICollection<object> o) where T : C => o.Where(e => e is T).Select(e => e as T);
+    }
+}";
+
+        private const string ExpectedOutput1 = @"
 using System.Collections.Generic;
 using System.Linq;
 
@@ -34,9 +46,22 @@ namespace Entry
     }
 }";
 
+        private const string ExpectedOutput2 = @"
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entry
+{
+    public class C
+    {
+        public static IEnumerable<T> Foo<T>(ICollection<object> o) where T : C => o.OfType<T>();
+    }
+}";
+
         public static IEnumerable<object[]> TestCases()
         {
-            yield return new object[] { Input, ExpectedOutput };
+            yield return new object[] { Input1, ExpectedOutput1 };
+            yield return new object[] { Input2, ExpectedOutput2 };
         }
     }
 }
